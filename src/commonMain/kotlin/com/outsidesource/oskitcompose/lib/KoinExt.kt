1bc8a6d9c4d56c15ec1087ccab8c5a9cb7247f9e
Ryan Mitchener <ryan@outsidesource.com>
2023-09-02 00:33:45 -0400

    Added rememberInject with keys

--- src/commonMain/kotlin/com/outsidesource/oskitcompose/lib/KoinExt.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitcompose/lib/KoinExt.kt
@@ -19,4 +19,31 @@ inline fun <reified T : Any> rememberInject(
     noinline parameters: ParametersDefinition? = null,
 ): T = remember { koinInjector.inject<T>(parameters = parameters).value }
 
+@Composable
+inline fun <reified T : Any> rememberInject(
+    key1: Any?,
+    noinline parameters: ParametersDefinition? = null,
+): T = remember(key1) { koinInjector.inject<T>(parameters = parameters).value }
+
+@Composable
+inline fun <reified T : Any> rememberInject(
+    key1: Any?,
+    key2: Any?,
+    noinline parameters: ParametersDefinition? = null,
+): T = remember(key1, key2) { koinInjector.inject<T>(parameters = parameters).value }
+
+@Composable
+inline fun <reified T : Any> rememberInject(
+    key1: Any?,
+    key2: Any?,
+    key3: Any?,
+    noinline parameters: ParametersDefinition? = null,
+): T = remember(key1, key2, key3) { koinInjector.inject<T>(parameters = parameters).value }
+
+@Composable
+inline fun <reified T : Any> rememberInject(
+    vararg keys: Any?,
+    noinline parameters: ParametersDefinition? = null,
+): T = remember(keys) { koinInjector.inject<T>(parameters = parameters).value }
+
 val koinInjector = object : KoinComponent {}
