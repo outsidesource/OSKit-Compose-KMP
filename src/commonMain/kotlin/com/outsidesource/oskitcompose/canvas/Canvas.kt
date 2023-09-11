ba0987f5ff9135345d3dee0ca241f46e975f6773
Ryan Mitchener <ryan@outsidesource.com>
2023-09-11 15:05:12 -0400

    Removed unnecessary KMPCanvasTypeface

--- src/commonMain/kotlin/com/outsidesource/oskitcompose/canvas/Canvas.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitcompose/canvas/Canvas.kt
@@ -1,39 +1,6 @@
 package com.outsidesource.oskitcompose.canvas
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Paint
-import com.outsidesource.oskitcompose.resources.KMPFont
-import com.outsidesource.oskitcompose.resources.KMPImage
-
-expect interface KMPTextLine {
-    val text: String
-    val width: Float
-    val height: Float
-    val ascent: Float
-    val descent: Float
-
-    companion object {
-        fun make(text: String, typeface: KMPCanvasTypeface, size: Float): KMPTextLine
-    }
-}
-
-interface KMPCanvasTypeface {
-    companion object
-}
-
-@Composable
-expect fun rememberKmpCanvasTypeface(font: KMPFont): KMPCanvasTypeface
-
-/**
- * Resolves a KMPCanvasTypeface from a KMPResource.
- * Note: Android KMPResources that use resource Ids (Int) will return the default typeface
- */
-expect suspend fun resolveKmpCanvasTypeface(font: KMPFont): KMPCanvasTypeface
-
-expect val KMPCanvasTypeface.Companion.Default: KMPCanvasTypeface
-
-expect fun Canvas.drawKmpTextLine(textLine: KMPTextLine, x: Float, y: Float, paint: Paint)
 
 expect fun Paint.kmpBlur(radius: Float, mode: KMPBlurMode = KMPBlurMode.Normal)
 
--- src/jvmMain/kotlin/com/outsidesource/oskitcompose/canvas/CanvasImp.kt
+++ src/jvmMain/kotlin/com/outsidesource/oskitcompose/canvas/CanvasImp.kt
@@ -1,65 +1,8 @@
 package com.outsidesource.oskitcompose.canvas
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.nativeCanvas
-import com.outsidesource.oskitcompose.resources.KMPFont
-import com.outsidesource.oskitcompose.resources.KMPImage
-import kotlinx.coroutines.runBlocking
-import org.jetbrains.skia.*
-
-private data class DesktopKMPCanvasTypeface(val font: Font) : KMPCanvasTypeface
-
-private data class DesktopKMPTextLine(
-    override val text: String,
-    override val width: Float,
-    override val height: Float,
-    override val ascent: Float,
-    override val descent: Float,
-    val textLine: TextLine,
-) : KMPTextLine
-
-private val defaultCanvasTypeface = DesktopKMPCanvasTypeface(Font(Typeface.makeDefault()))
-
-actual val KMPCanvasTypeface.Companion.Default: KMPCanvasTypeface
-    get() = defaultCanvasTypeface
-
-@Composable
-actual fun rememberKmpCanvasTypeface(font: KMPFont): KMPCanvasTypeface {
-    return remember(font) {
-        runBlocking { resolveKmpCanvasTypeface(font) }
-    }
-}
-
-actual suspend fun resolveKmpCanvasTypeface(font: KMPFont): KMPCanvasTypeface {
-    val bytes = font.readBytes()
-    return DesktopKMPCanvasTypeface(Font(Typeface.makeFromData(Data.makeFromBytes(bytes))))
-}
-
-actual interface KMPTextLine {
-    actual val text: String
-    actual val width: Float
-    actual val height: Float
-    actual val ascent: Float
-    actual val descent: Float
-
-    actual companion object {
-        actual fun make(text: String, typeface: KMPCanvasTypeface, size: Float): KMPTextLine {
-            val bounds = TextLine.make(text, (typeface as DesktopKMPCanvasTypeface).font.makeWithSize(size))
-
-            return DesktopKMPTextLine(
-                text,
-                bounds.width, bounds.height, ascent = bounds.ascent, descent = bounds.descent, bounds
-            )
-        }
-    }
-}
-
-actual fun Canvas.drawKmpTextLine(textLine: KMPTextLine, x: Float, y: Float, paint: Paint) {
-    nativeCanvas.drawTextLine((textLine as DesktopKMPTextLine).textLine, x, y, paint.asFrameworkPaint())
-}
+import org.jetbrains.skia.FilterBlurMode
+import org.jetbrains.skia.MaskFilter
 
 actual fun Paint.kmpBlur(radius: Float, mode: KMPBlurMode) {
     val frameworkPaint = asFrameworkPaint()
