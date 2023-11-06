b6f7dfd11faa129f2272219bffdb459d3e8267a6
Ryan Mitchener <ryan@outsidesource.com>
2023-11-05 23:24:23 -0500

    KMPDeepLinkEffect as experimental

--- src/commonMain/kotlin/com/outsidesource/oskitcompose/deeplink/KMPDeepLinkUI.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitcompose/deeplink/KMPDeepLinkUI.kt
@@ -1,8 +1,10 @@
 package com.outsidesource.oskitcompose.deeplink
 
 import androidx.compose.runtime.Composable
+import com.outsidesource.oskitkmp.annotation.ExperimentalOSKitAPI
 import com.outsidesource.oskitkmp.deeplink.KMPDeepLink
 
+@ExperimentalOSKitAPI
 @Composable
 expect fun KMPDeepLinkEffect(
     initialDeepLink: KMPDeepLink?,
