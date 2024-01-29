6393b030f7359c8022bc0324ae0adbde5161e684
Ryan Mitchener <ryan@outsidesource.com>
2024-01-29 00:28:38 -0500

    Fixed iOS Window Insets

--- src/iosMain/kotlin/com/outsidesource/oskitcompose/systemui/iOSKMPWindowInsets.kt
+++ src/iosMain/kotlin/com/outsidesource/oskitcompose/systemui/iOSKMPWindowInsets.kt
@@ -1,35 +1,32 @@
 package com.outsidesource.oskitcompose.systemui
 
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 
 
 actual val KMPWindowInsets.Companion.topInsets: WindowInsets
     @Composable
-    get() = LocalKMPWindowInsets.current?.topInsets ?: WindowInsets.statusBars
+    get() = LocalKMPWindowInsets.current?.topInsets ?: WindowInsets.systemBars.only(WindowInsetsSides.Top)
 
 actual val KMPWindowInsets.Companion.bottomInsets: WindowInsets
     @Composable
-    get() = LocalKMPWindowInsets.current?.bottomInsets ?: WindowInsets.navigationBars
+    get() = LocalKMPWindowInsets.current?.bottomInsets ?: WindowInsets.systemBars.only(WindowInsetsSides.Bottom)
 
 actual val KMPWindowInsets.Companion.verticalInsets: WindowInsets
     @Composable
-    get() = LocalKMPWindowInsets.current?.verticalInsets ?: WindowInsets.statusBars
+    get() = LocalKMPWindowInsets.current?.verticalInsets ?: WindowInsets.systemBars.only(WindowInsetsSides.Vertical)
 
 actual val KMPWindowInsets.Companion.rightInsets: WindowInsets
     @Composable
-    get() = LocalKMPWindowInsets.current?.rightInsets ?: DefaultKMPWindowInsets
+    get() = LocalKMPWindowInsets.current?.rightInsets ?: WindowInsets.systemBars.only(WindowInsetsSides.Right)
 
 actual val KMPWindowInsets.Companion.leftInsets: WindowInsets
     @Composable
-    get() = LocalKMPWindowInsets.current?.leftInsets ?: DefaultKMPWindowInsets
+    get() = LocalKMPWindowInsets.current?.leftInsets ?: WindowInsets.systemBars.only(WindowInsetsSides.Left)
 
 actual val KMPWindowInsets.Companion.horizontalInsets: WindowInsets
     @Composable
-    get() = LocalKMPWindowInsets.current?.horizontalInsets ?: DefaultKMPWindowInsets
+    get() = LocalKMPWindowInsets.current?.horizontalInsets ?: WindowInsets.systemBars.only(WindowInsetsSides.Horizontal)
 
 actual val KMPWindowInsets.Companion.allInsets: WindowInsets
     @Composable
