d5172050adc577e03421f1569ba01c519f3c9133
Ryan Mitchener <ryan@outsidesource.com>
2024-04-25 09:42:49 -0400

    Added help documentation

--- src/commonMain/kotlin/com/outsidesource/oskitcompose/systemui/KMPDisableScreenIdleTimeoutEffect.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitcompose/systemui/KMPDisableScreenIdleTimeoutEffect.kt
@@ -3,7 +3,7 @@ package com.outsidesource.oskitcompose.systemui
 import androidx.compose.runtime.Composable
 
 /**
- * 
+ * Disables the Screen Idle Timeout on Android and iOS preventing the screen from sleeping
  */
 @Composable
 expect fun KMPDisableScreenIdleTimeoutEffect(isEnabled: Boolean = true)
