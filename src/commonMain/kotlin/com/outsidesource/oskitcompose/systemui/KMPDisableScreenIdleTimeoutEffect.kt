d84e11f4834950205b246aa2d42c85ff86685b0b
Ryan Mitchener <ryan@outsidesource.com>
2024-11-27 21:43:25 -0500

    Added DisableScreenIdleTimeoutEffect for WASM

--- src/commonMain/kotlin/com/outsidesource/oskitcompose/systemui/KMPDisableScreenIdleTimeoutEffect.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitcompose/systemui/KMPDisableScreenIdleTimeoutEffect.kt
@@ -3,7 +3,12 @@ package com.outsidesource.oskitcompose.systemui
 import androidx.compose.runtime.Composable
 
 /**
- * Disables the Screen Idle Timeout on Android and iOS preventing the screen from sleeping
+ * Disables the Screen Idle Timeout preventing the screen from sleeping
+ *
+ * Android: Supported
+ * iOS: Supported
+ * JVM: Not supported
+ * Web: Supported on all browsers that support navigator.wakeLock
  */
 @Composable
 expect fun KMPDisableScreenIdleTimeoutEffect(isEnabled: Boolean = true)
--- src/wasmJsMain/kotlin/com/outsidesource/oskitcompose/systemui/KMPDisableScreenIdleTimeoutEffect.wasm.kt
+++ src/wasmJsMain/kotlin/com/outsidesource/oskitcompose/systemui/KMPDisableScreenIdleTimeoutEffect.wasm.kt
@@ -1,8 +1,55 @@
 package com.outsidesource.oskitcompose.systemui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import kotlinx.coroutines.*
+import kotlin.js.Promise
 
+private val scope = CoroutineScope(Dispatchers.Default + SupervisorJob())
+
+/***
+ *
+ */
 @Composable
 actual fun KMPDisableScreenIdleTimeoutEffect(isEnabled: Boolean) {
-    // TODO: WASM
+    if (!isEnabled) return
+
+    DisposableEffect(Unit) {
+        var wakeLock: WakeLockSentinel? = null
+
+        scope.launch {
+            try {
+                wakeLock = requestWakeLock().await<WakeLockSentinel?>()
+            } catch (t: Throwable) {
+                // Ignore
+            }
+        }
+
+        onDispose {
+            scope.launch {
+                try {
+                    wakeLock?.release()?.await<JsAny?>()
+                } catch (t: Throwable) {
+                    // Ignore
+                }
+            }
+        }
+    }
+}
+
+private fun requestWakeLock(): Promise<WakeLockSentinel?> = js(
+    """{
+        try {
+            if (navigator["wakeLock"] === undefined) return Promise.resolve(null);
+            return navigator.wakeLock.request("screen");
+        } catch (err) {
+            return Promise.resolve(null);
+        }
+    }"""
+)
+
+private external interface WakeLockSentinel : JsAny {
+    val released: Boolean
+    val type: String
+    fun release(): Promise<JsAny?>
 }
