9851c0b27ae0b50241af4aeedbb47d69ff8dfc58
Ryan Mitchener <ryan@outsidesource.com>
2025-01-24 13:54:34 -0500

    Formatting updates

--- src/wasmJsMain/kotlin/com/outsidesource/oskitcompose/canvas/Painter.wasm.kt
+++ src/wasmJsMain/kotlin/com/outsidesource/oskitcompose/canvas/Painter.wasm.kt
@@ -7,5 +7,8 @@ import androidx.compose.ui.unit.Density
 import okio.BufferedSource
 import org.jetbrains.skia.Image
 
-actual fun kmpLoadImageBitmap(source: BufferedSource): ImageBitmap = Image.makeFromEncoded(source.readByteArray()).toComposeImageBitmap()
+actual fun kmpLoadImageBitmap(source: BufferedSource): ImageBitmap =
+    Image.makeFromEncoded(source.readByteArray()).toComposeImageBitmap()
+
+
 actual fun kmpLoadSvgPainter(source: BufferedSource, density: Density): Painter = ImageLoadErrorPainter(density)
