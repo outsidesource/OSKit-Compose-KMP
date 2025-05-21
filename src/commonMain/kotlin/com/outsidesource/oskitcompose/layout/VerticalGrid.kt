bfa8ebb5d7f888b78c4ef7819c1767bb1f672e88
Ryan Mitchener <ryan@outsidesource.com>
2025-05-21 00:46:48 -0400

    Updates to vertical grid

--- src/commonMain/kotlin/com/outsidesource/oskitcompose/layout/VerticalGrid.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitcompose/layout/VerticalGrid.kt
@@ -5,13 +5,11 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.Placeable
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
-import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
@@ -20,7 +18,7 @@ import kotlin.math.max
 
 @Composable
 fun VerticalGrid(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     columns: Int,
     horizontalArrangement: Arrangement.Horizontal = Arrangement.Start,
     verticalArrangement: Arrangement.Vertical = Arrangement.Top,
@@ -28,18 +26,8 @@ fun VerticalGrid(
     layoutDirection: LayoutDirection = LocalLayoutDirection.current,
     content: @Composable () -> Unit,
 ) {
+    require(columns > 0) { "columns must be > 0, was $columns" }
     val density = LocalDensity.current
-    val localHArrangement = remember<(Int, IntArray, LayoutDirection, Density, IntArray) -> Unit>(density, horizontalArrangement) {
-        { totalSize, sizes, layoutDirection, density, outPosition ->
-            with(horizontalArrangement) { density.arrange(totalSize, sizes, layoutDirection, outPosition) }
-        }
-    }
-
-    val localVArrangement = remember<(Int, IntArray, Density, IntArray) -> Unit>(density, verticalArrangement) {
-        { totalSize, sizes, density, outPosition ->
-            with(verticalArrangement) { density.arrange(totalSize, sizes, outPosition) }
-        }
-    }
 
     Layout(
         modifier = modifier,
@@ -47,14 +35,16 @@ fun VerticalGrid(
     ) { measurables, constraints ->
         val placeables = arrayOfNulls<Placeable>(measurables.size)
         val rowHeights = IntArray(ceil(measurables.size / columns.toFloat()).toInt())
+        val rowCount = ceil(measurables.size / columns.toFloat()).toInt()
         val hSpacing = (horizontalArrangement.spacing * (columns - 1)).roundToPx()
-        val vSpacing = (((measurables.size / columns) - 1) * verticalArrangement.spacing).roundToPx()
+        val vSpacing = ((rowCount - 1) * verticalArrangement.spacing).roundToPx()
         val startPadding = contentPadding.calculateStartPadding(layoutDirection).roundToPx()
         val topPadding = contentPadding.calculateTopPadding().roundToPx()
         val hPadding = startPadding + contentPadding.calculateEndPadding(layoutDirection).roundToPx()
         val vPadding = topPadding + contentPadding.calculateBottomPadding().roundToPx()
+        val cellWidth = (constraints.maxWidth - hSpacing - hPadding) / columns
 
-        for (i in 0..< ceil(measurables.size / columns.toFloat()).toInt()) {
+        for (i in 0..< rowCount) {
             var maxItemHeight = 0
 
             for (j in 0..< columns) {
@@ -62,11 +52,7 @@ fun VerticalGrid(
                 if (itemIndex >= measurables.size) break
                 val measurable = measurables[itemIndex]
                 val placeable = measurable.measure(
-                    constraints.copy(
-                        maxWidth = (constraints.maxWidth - hSpacing - hPadding) / columns,
-                        minWidth = (constraints.maxWidth - hSpacing - hPadding) / columns,
-                        minHeight = 0
-                    )
+                    constraints.copy(maxWidth = cellWidth, minWidth = cellWidth, minHeight = 0)
                 )
                 placeables[itemIndex] = placeable
                 maxItemHeight = max(placeable.height, maxItemHeight)
@@ -78,14 +64,18 @@ fun VerticalGrid(
         val layoutHeight = max(constraints.minHeight, rowHeights.sumOf { it } + vSpacing + vPadding)
 
         layout(layoutWidth, layoutHeight) {
-            val yPositions = IntArray(ceil(placeables.size / columns.toFloat()).toInt())
+            val yPositions = IntArray(rowCount)
+            with(verticalArrangement) {
+                density.arrange(layoutHeight - vPadding, rowHeights, yPositions)
+            }
 
-            for (i in 0..< ceil(placeables.size / columns.toFloat()).toInt()) {
+            for (i in 0..< rowCount) {
                 val xPositions = IntArray(columns)
                 val childrenWidths = IntArray(columns) { j -> placeables.getOrNull((i * columns) + j)?.width ?: 0 }
 
-                localHArrangement(layoutWidth, childrenWidths, layoutDirection, density, xPositions)
-                localVArrangement(layoutHeight - vPadding, rowHeights, density, yPositions)
+                with(horizontalArrangement) {
+                    density.arrange(layoutWidth, childrenWidths, layoutDirection, xPositions)
+                }
 
                 for (j in 0..< columns) {
                     val itemIndex = (i * columns) + j
