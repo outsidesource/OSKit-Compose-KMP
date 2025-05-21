32ca04dcc37c36b0172a40ec29ee248ffea173b0
Ryan Mitchener <ryan@outsidesource.com>
2025-05-21 00:33:57 -0400

    Added package for VerticalGrid

--- src/commonMain/kotlin/com/outsidesource/oskitcompose/layout/VerticalGrid.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitcompose/layout/VerticalGrid.kt
@@ -1,3 +1,5 @@
+package com.outsidesource.oskitcompose.layout
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
