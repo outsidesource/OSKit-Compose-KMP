f0684c744ff883ecbeab322614ab2234db5c4359
Ryan Mitchener <ryan@outsidesource.com>
2025-08-08 23:57:44 -0400

    Added drawInnerShadow and drawOuterShadow

--- src/commonMain/kotlin/com/outsidesource/oskitcompose/modifier/Shadow.kt
+++ src/commonMain/kotlin/com/outsidesource/oskitcompose/modifier/Shadow.kt
@@ -6,7 +6,10 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.unit.Dp
@@ -24,35 +27,47 @@ fun Modifier.innerShadow(
 ) = (if (!drawOverContent) graphicsLayer { alpha = .99f } else this) // This forces Android to use alpha compositing
     .drawWithContent {
         if (drawOverContent) drawContent()
+        drawInnerShadow(blur = blur, spread = spread, color = color, shape = shape, offset = offset)
+        if (!drawOverContent) drawContent()
+    }
 
-        val rect = Rect(Offset.Zero, size)
+fun DrawScope.drawInnerShadow(
+    topLeft: Offset = Offset.Zero,
+    size: Size = this.size,
+    blur: Dp,
+    spread: Dp = 0.dp,
+    color: Color = Color.Black,
+    shape: Shape = RectangleShape,
+    offset: DpOffset = DpOffset.Zero,
+) {
+    drawIntoCanvas {
         val paint = Paint()
+        paint.color = color
+        paint.isAntiAlias = true
 
-        drawIntoCanvas {
-            paint.color = color
-            paint.isAntiAlias = true
-
-            if (drawOverContent) it.saveLayer(rect, paint)
-
-            val fillOutline = shape.createOutline(size, layoutDirection, this)
-            it.drawOutline(paint = paint, outline = fillOutline)
+        it.withSaveLayer(bounds = Rect(topLeft, size), paint = paint) {
+            translate(
+                left = topLeft.x,
+                top = topLeft.y,
+            ) {
+                val fillOutline = shape.createOutline(size, layoutDirection, this)
+                it.drawOutline(paint = paint, outline = fillOutline)
+            }
 
             paint.blendMode = BlendMode.DstOut
-
             if (blur.toPx() > 0) paint.kmpBlur(blur.toPx() / 2)
 
             translate(
-                left = spread.toPx() + offset.x.toPx(),
-                top = spread.toPx() + offset.y.toPx()
+                left = topLeft.x + spread.toPx() + offset.x.toPx(),
+                top = topLeft.y + spread.toPx() + offset.y.toPx()
             ) {
                 val shadowSize = size.copy(width = size.width - (spread.toPx() * 2), size.height - (spread.toPx() * 2))
                 val shadowOutline = shape.createOutline(shadowSize, layoutDirection, this)
                 it.drawOutline(paint = paint, outline = shadowOutline)
             }
         }
-
-        if (!drawOverContent) drawContent()
     }
+}
 
 data class InnerShadow(
     val blur: Dp,
@@ -70,6 +85,18 @@ fun Modifier.outerShadow(
     shape: Shape = RectangleShape,
     offset: DpOffset = DpOffset.Zero,
 ) = drawBehind {
+    drawOuterShadow(blur = blur, spread = spread, color = color, shape = shape, offset = offset)
+}
+
+fun DrawScope.drawOuterShadow(
+    topLeft: Offset = Offset.Zero,
+    size: Size = this.size,
+    blur: Dp,
+    spread: Dp = 0.dp,
+    color: Color = Color.Black,
+    shape: Shape = RectangleShape,
+    offset: DpOffset = DpOffset.Zero,
+) {
     val paint = Paint()
 
     drawIntoCanvas {
@@ -81,8 +108,8 @@ fun Modifier.outerShadow(
         val outline = shape.createOutline(shadowSize, layoutDirection, this)
 
         translate(
-            left = -(spread.toPx()) + offset.x.toPx(),
-            top = -(spread.toPx()) + offset.y.toPx()
+            left = topLeft.x + -(spread.toPx()) + offset.x.toPx(),
+            top = topLeft.y + -(spread.toPx()) + offset.y.toPx(),
         ) {
             it.drawOutline(paint = paint, outline = outline)
         }
