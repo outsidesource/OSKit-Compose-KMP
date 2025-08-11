88d8e9b9614d1d06acbf93cb9d8482fdd864a114
Ryan Mitchener <ryan@outsidesource.com>
2025-08-11 15:12:01 -0400

    Beta2

--- build.gradle.kts
+++ build.gradle.kts
@@ -163,7 +163,7 @@ android {
 }
 
 mavenPublishing {
-    publishToMavenCentral(SonatypeHost.S01, automaticRelease = true)
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
     signAllPublications()
 
     configure(
